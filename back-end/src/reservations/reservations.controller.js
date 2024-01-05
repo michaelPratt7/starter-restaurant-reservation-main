24cea59d63522e32f6fbb2fabac39f434c691647
Michael Pratt <mrpratt7751@gmail.com>
2024-01-04 20:21:28 -0500

    validationMiddleware

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -10,6 +10,65 @@ async function list(req, res) {
   res.json({data: await service.list(req.query.date)});
 }
 
+function create(req, res) {
+  service
+    .create(req.body.data)
+    .then((data) => res.status(201).json({ data }));
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Order must include a ${propertyName}` 
+  });
+  };
+}
+
+function propertyIsNotEmpty(propertyName) {
+  return function (req, res, next) {
+  const {data = {}} = req.body;
+  if (data[propertyName] == "") {
+      return next ({
+          status: 400,
+          message: `Order must include a ${propertyName}`
+      })
+  };
+  next();
+  };
+}
+
+function reservationDateIsValid() {
+  return function (req, res, next) {
+  const reservationDate = new Date(req.body.reservation_date);
+
+    if (isNaN(reservationDate.getTime())) {
+      return res.status(400).json({ error: 'Invalid reservation_date format' });
+    }
+    next();
+  }
+}
+
 module.exports = {
   list: [asyncErrorBoundary(list)],
+  create: [
+    bodyDataHas("first_name"),
+    bodyDataHas("last_name"),
+    bodyDataHas("mobile_number"),
+    bodyDataHas("reservation_date"),
+    bodyDataHas("reservation_time"),
+    bodyDataHas("people"),
+    reservationDateIsValid,
+    propertyIsNotEmpty("first_name"),
+    propertyIsNotEmpty("last_name"),
+    propertyIsNotEmpty("reservation_date"),
+    propertyIsNotEmpty("reservation_time"),
+    propertyIsNotEmpty("people"),
+    asyncErrorBoundary(create),
+  ],
+
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,9 +1,19 @@
 const knex = require ("../db/connection");
 
 function list(date) {
-    return knex('reservations').whereRaw('DATE(reservation_date) = ?', [date]).select('*');
+    return knex("reservations")
+        .whereRaw("DATE(reservation_date) = ?", [date])
+        .orderBy("reservation_time", "asc")
+        .select("*");
+}
+
+function create(reservation) {
+    return knex("reservations")
+        .insert(reservation)
+        .returning("*")
 }
 
 module.exports = {
     list,
+    create,
 }
