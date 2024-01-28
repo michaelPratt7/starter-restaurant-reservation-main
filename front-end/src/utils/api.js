f122c9e4c89e63b5256751666a78e11f6500644f
Michael Pratt <mrpratt7751@gmail.com>
2024-01-28 17:34:13 -0500

    US5finishTableApi

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -109,7 +109,7 @@ export async function updateTable(reservation_id, table_id, signal) {
     return await fetchJson(url, options); 
   }
 
-  export async function finishTable(table_id, signal) {
+  export async function finishTable(reservation_date_id, table_id, signal) {
     const url = `${API_BASE_URL}/tables/${table_id}/seat`;
     const options = { method: "DELETE"};
     return await fetchJson(url, options, signal)
